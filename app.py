f3f1956c7f917f1e6862a8e107163bb393e99b4a
xiluo_plus <12551146+xiluo-plus@user.noreply.gitee.com>
2025-03-06 16:48:43 +0800

    database section

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import click
 from faker import Faker
-
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:926472@localhost:3306/movie'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24).hex()
 db = SQLAlchemy(app)
-
 # 定义模型
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -75,17 +75,55 @@ def forge():
         click.echo(f'Error: {e}')
 
 # 根路由
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    user = User.query.first()
-    movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    if request.method == 'POST':
+        title=request.form['title']
+        year=request.form['year']
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input')
+            return redirect(url_for('index'))
+        movie=Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Movie added!')
+        return redirect(url_for('index'))
+    movies=Movie.query.all()
+    return render_template('index.html',movies=movies)
 
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie=Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('has deleted.')
+    return redirect(url_for('index'))
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie=Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title=request.form['title']
+        year=request.form['year']
+        if not title or not year or len(year)!=4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie.title=title
+        movie.year=year
+        db.session.commit()
+        flash('Movie updated')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+
+
+@app.context_processor
+def inject_user():
+    user=User.query.first()
+    return dict(user=user)
 # 404 错误处理
 @app.errorhandler(404)
 def page_not_found(e):
-    user = User.query.first()
-    return render_template('404.html', user=user), 404
+    return render_template('404.html'), 404
 
 # 添加电影的路由
 @app.route('/add')
